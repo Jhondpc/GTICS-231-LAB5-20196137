58c20f11e6bfabe9467d2111587c01db308ccdfc
Jhondpc <a20196137@pucp.edu.pe>
2023-04-30 17:03:06 -0500

    proyecto creado

---
+++ src/main/java/com/example/gtics231lab520196137/Gtics231Lab520196137Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics231lab520196137;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gtics231Lab520196137Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gtics231Lab520196137Application.class, args);
+    }
+
+}
